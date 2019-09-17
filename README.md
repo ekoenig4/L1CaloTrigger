ae48ae21b6a3a269175c29cddbcb12e6a24e236c
ekoenig4 <ekoenig4@wisc.edu>
2019-09-17 12:28:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# L1CaloTrigger Phase 2 Jet Algorithm for CMSSW
+
+# Step-1: Tylers Code:
+````
+cmsrel CMSSW_10_5_0_pre1
+cd CMSSW_10_5_0_pre1/src
+cmsenv
+git cms-init
+git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+git fetch cms-l1t-offline phase2-l1t-integration-CMSSW_10_5_0_pre1
+git cms-merge-topic -u cms-l1t-offline:l1t-phase2-v2.17.15.1
+
+
+
+git cms-addpkg L1Trigger/L1TCommon
+
+scram b -j 8
+
+git remote add truggles git@github.com:truggles/cmssw.git
+git fetch truggles phase2-l1taus_10_5_Xv2
+git cms-merge-topic -u truggles:phase2-l1taus_10_5_X_Apr26
+````
+More information can be found here
+https://twiki.cern.ch/twiki/bin/viewauth/CMS/Phase2L1CaloJetsAndTaus#Central_Integration_Branch
+
+# Step-2: Phase 2 Jet Algorithm Code
+````
+cd $CMSSW_BASE/src/L1Trigger/
+rm -rf L1CaloTrigger/
+git clone https://github.com/ekoenig4/L1CaloTrigger.git
+````
+
+# Step-3: Crab Jobs
+Follow instructions for Tylers code
+https://github.com/truggles/L1EGRateStudies/tree/10_5_X_Taus
---
+++ plugins/L1EGammaEEProducer.cc
@@ -0,0 +1,66 @@
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/L1THGCal/interface/HGCalMulticluster.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/L1Trigger/interface/EGamma.h"
+#include "L1Trigger/L1CaloTrigger/interface/L1EGammaEECalibrator.h"
+
+
+class L1EGammaEEProducer : public edm::EDProducer {
+   public:
+      explicit L1EGammaEEProducer(const edm::ParameterSet&);
+
+   private:
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+
+      edm::EDGetToken multiclusters_token_;
+      L1EGammaEECalibrator calibrator_;
+};
+
+L1EGammaEEProducer::L1EGammaEEProducer(const edm::ParameterSet& iConfig) :
+  multiclusters_token_(consumes<l1t::HGCalMulticlusterBxCollection>(iConfig.getParameter<edm::InputTag>("Multiclusters"))),
+  calibrator_(iConfig.getParameter<edm::ParameterSet>("calibrationConfig")) {
+    produces< BXVector<l1t::EGamma> >("L1EGammaCollectionBXVWithCuts");
+}
+
+
+void L1EGammaEEProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  float minEt_ = 0;
+
+  std::unique_ptr< BXVector<l1t::EGamma> > l1EgammaBxCollection(new l1t::EGammaBxCollection);
+
+
+  // retrieve clusters 3D
+  edm::Handle<l1t::HGCalMulticlusterBxCollection> multiclusters_h;
+  iEvent.getByToken(multiclusters_token_, multiclusters_h);
+  const l1t::HGCalMulticlusterBxCollection& multiclusters = *multiclusters_h;
+
+  // here we loop on the TPGs
+  for(auto cl3d = multiclusters.begin(0); cl3d != multiclusters.end(0); cl3d++) {
+     // std::cout << "-- CL3D is EG: " <<  cl3d->hwQual() << "   "<< cl3d->eta() <<"   "<< cl3d->pt()<<std::endl;
+     if(cl3d->hwQual()) {
+       if(cl3d->et() > minEt_) {
+         int hw_quality = 1; // baseline EG ID passed
+         if(fabs(cl3d->eta()) >= 1.52) {
+           hw_quality = 2; // baseline EG ID passed + cleanup of transition region
+         }
+
+         float calib_factor = calibrator_.calibrationFactor(cl3d->pt(), cl3d->eta());
+         l1t::EGamma eg=l1t::EGamma(reco::Candidate::PolarLorentzVector(cl3d->pt()/calib_factor, cl3d->eta(), cl3d->phi(), 0.));
+         eg.setHwQual(hw_quality);
+         eg.setHwIso(1);
+         eg.setIsoEt(-1); // just temporarily as a dummy value 
+         l1EgammaBxCollection->push_back(0,eg);
+      }
+    }
+  }
+
+  iEvent.put(std::move(l1EgammaBxCollection),"L1EGammaCollectionBXVWithCuts");
+}
+
+
+
+DEFINE_FWK_MODULE(L1EGammaEEProducer);
